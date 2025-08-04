dfbf702852ed60beefb7217e24eb2e2c6d73437e
Nadia Santalla <nadia@santalla.io>
2025-08-04 10:31:10 +0200

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
+	kubenodelabeler "github.com/grafana/kube-node-labeler"
+	"github.com/grafana/kube-node-labeler/config"
+	"github.com/grafana/kube-node-labeler/metrics"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/sync/errgroup"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var log = slog.Default()
+
+func run() error {
+	levelStr := flag.String("log-level", slog.LevelInfo.String(), "slog level")
+	configPath := flag.String("config", "", "path to config file")
+	flag.Parse()
+
+	var lvl slog.Level
+	err := lvl.UnmarshalText([]byte(*levelStr))
+	if err != nil {
+		return fmt.Errorf("parsing log level: %w", err)
+	}
+
+	log = slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: lvl}))
+
+	if *configPath == "" {
+		return errors.New("path to config must be provided, see -h")
+	}
+
+	configFile, err := os.Open(*configPath)
+	if err != nil {
+		return fmt.Errorf("reading config file: %w", err)
+	}
+
+	defer configFile.Close() //nolint:errcheck
+
+	cfg, err := config.Read(configFile)
+	if err != nil {
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	kconfig, err := rest.InClusterConfig()
+	if err != nil {
+		return fmt.Errorf("reading in-cluster config: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(kconfig)
+	if err != nil {
+		return fmt.Errorf("creating clientset: %w", err)
+	}
+
+	reg := prometheus.NewRegistry()
+	handler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
+
+	mux := http.NewServeMux()
+	mux.Handle("GET /metrics", handler)
+
+	nl := &kubenodelabeler.NodeLabeler{
+		Log:           log,
+		Metrics:       metrics.New(prometheus.NewRegistry()), // FIXME: Pass prometheus registry
+		KubeClient:    clientset,
+		ConfigEntries: cfg.Entries,
+	}
+
+	ctx := context.Background()
+	// TODO: Catch signal, cancel context.
+
+	eg := errgroup.Group{}
+	eg.Go(func() error {
+		return nl.Start(ctx)
+	})
+	eg.Go(func() error {
+		return http.ListenAndServe(cfg.MetricsAddr, mux)
+	})
+
+	return eg.Wait()
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Error("running application", "err", err)
+		os.Exit(1)
+	}
+}
